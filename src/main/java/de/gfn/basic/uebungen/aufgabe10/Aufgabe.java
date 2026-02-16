cf00360facf4e4158dc6fa6d2028238ac3086095
tlubowiecki <tomasz@lubowiecki.de>
2026-02-16 14:00:22 +0100

    Aufgabe

---
+++ src/main/java/de/gfn/basic/uebungen/aufgabe10/Aufgabe.java
@@ -0,0 +1,15 @@
+package de.gfn.basic.uebungen.aufgabe10;
+
+public class Aufgabe {
+
+    /*
+
+    Schreibe für jedes der folgenden BuildIn-FunctionalInterfaces 3 Varianten.
+    Consumer, Function, Predicate, Supplier, UnaryOperator
+    Verwende unterschiedliche Datentypen.
+
+    Schreibe 2 eigene FunctionalInterfaces und implementiere sie jeweils 3 mal.
+
+     */
+
+}
